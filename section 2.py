9ce98dd252234315a05935ca634329e3804fc393
usmankvso <81707457+usmankvso@users.noreply.github.com>
2021-04-04 23:09:47 +0100

    Add files via upload

---
+++ section 2.py
@@ -0,0 +1,35 @@
+print("QUESTION 1")
+print("My name is USMAN")
+print("Computer Science Department")
+
+
+print("QUESTION 2")
+scare = 'Boo'
+print(scare)
+
+
+print("QUESTION 3")
+number_one = 10
+number_two = 12
+number_three = number_one + number_two
+print(number_three)
+
+
+print("QUESTION 4")
+Dec = 12 / 0.5
+print(Dec)
+
+
+print("QUESTION 5")
+add = 12 + 4
+print(add)
+
+
+print("QUESTION 6")
+modulus = 8 % 3
+print(modulus)
+
+
+print("QUESTION 7")
+result = (2 * 4) + (4 + 2)
+print(result)
